d277f54b8f5df424af35a3a75740dd3b8a56c491
hikoand <71857799+hikoand@users.noreply.github.com>
2021-05-21 00:19:50 +0900

    bg-animation
    bg-animation

--- js/common.js
+++ js/common.js
@@ -1,12 +1,12 @@
 $(function () {
-  $("nav ul li a,.back_to_top a,a.button,.footer_top a").click(function () {
+  $('nav ul li a,.back_to_top a,a.button,.footer_top a').click(function () {
     // console.log($(this.hash))
 
     let thisElem = $(this.hash);
     let offsetElem = thisElem.offset();
 
-    $("html,body").stop();
-    $("html,body").animate({ scrollTop: offsetElem.top }, 1500);
+    $('html,body').stop();
+    $('html,body').animate({ scrollTop: offsetElem.top }, 1500);
   });
 
   //go to top buttom display
@@ -14,43 +14,50 @@ $(function () {
     // console.log($(this).scrollTop())
 
     if ($(this).scrollTop() == 0) {
-      $(".back_to_top").removeClass("on");
+      $('.back_to_top').removeClass('on');
     } else {
-      $(".back_to_top").addClass("on");
+      $('.back_to_top').addClass('on');
     }
   });
 
   //section offset top 값으로 add class
   let wHeight = $(window).innerHeight();
-  console.log("window height: " + wHeight);
+  console.log('window height: ' + wHeight);
   $(window).scroll(function () {
     let thisScrollTop = $(this).scrollTop();
-    $("section").each(function () {
+    $('section').each(function () {
       let thisOffset = $(this).offset();
       if (
         thisOffset.top <= thisScrollTop &&
         thisScrollTop < thisOffset.top + wHeight
       ) {
-        $(this).addClass("active");
+        $(this).addClass('active');
       }
     });
   });
+
+  var timer = setTimeout(function () {
+    $('#header, #intro img,.title_1,.title_2,.title_3,.intro_right').addClass(
+      'aniOn'
+    );
+    $('.title_2').toggleClass('changed');
+  }, 1500);
 });
 
 //iframe api
-var tag = document.createElement("script");
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 //플레이어 변수 설정
 var player;
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player("player", {
-    height: "400",
-    width: "620",
-    videoId: "9lRDo1XjV9U",
+  player = new YT.Player('player', {
+    height: '400',
+    width: '620',
+    videoId: '9lRDo1XjV9U',
     playerVars: {
       setVolume: 0,
       rel: 0,
@@ -68,8 +75,7 @@ function onYouTubeIframeAPIReady() {
 }
 
 function onPlayerReady(event) {
-  //로딩된 후에 실행될 동작
-  event.target.playVideo(); //자동재생
+  event.target.playVideo();
 }
 
 var done = false;
@@ -77,6 +83,32 @@ var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
     done = true;
-    //플레이어가 재생중일 때 작성한 동작이 실행된다.
   }
 }
+
+//timer
+function countDday() {
+  var dday = new Date('Jun 11,2021,23:59:59').getTime(); //디데이
+  setInterval(function () {
+    var now = new Date(); //현재 날짜 가져오기
+    var distance = dday - now;
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (distance < 0) {
+      $('.timer_inner').html('마감되었습니다.');
+      $('#conut_list').hide();
+    }
+
+    if (s < 10) {
+      s = '0' + s;
+    }
+    $('#days').html(d + '.');
+    $('#hour').html(h + '.');
+    $('#mimutes').html(m + '.');
+    $('#seconds').html(s);
+  }, 1000);
+}
+countDday();
